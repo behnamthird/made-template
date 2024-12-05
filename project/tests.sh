2adce8f9873aa519c4a2c1c77276e59151b848fb
Behnam Ghezelbashi <153480843+behnamthird@users.noreply.github.com>
2024-12-05 13:25:17 +0100

    tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+
+echo "Running tests..."
+
+
+pytest tests
+
+
+if [ $? -eq 0 ]; then
+    echo "All tests passed successfully!"
+else
+    echo "Tests failed! Check logs for details."
+    exit 1
+fi
